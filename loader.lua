2af99207a09ed8bcbdceba2052bafe645f80d920
Lava <69821200+RealLava@users.noreply.github.com>
2023-01-17 12:05:32 -0800

    Update loader.lua

---
+++ loader.lua
@@ -0,0 +1,33 @@
+--[[
+  ______ _  ___   ___      _    _____                      _      
+ |  ____| |/ _ \ / _ \    | |  / ____|                    | |     
+ | |__  | | | | | | | | __| | | |     ___  _ __  ___  ___ | | ___ 
+ |  __| | | | | | | | |/ _` | | |    / _ \| '_ \/ __|/ _ \| |/ _ \
+ | |    | | |_| | |_| | (_| | | |___| (_) | | | \__ \ (_) | |  __/
+ |_|    |_|\___/ \___/ \__,_|  \_____\___/|_| |_|___/\___/|_|\___|
+                                                                                                                                  
+--]]
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/RealLava/fl00dhub/main/assets/discord.lua"))()
+task.wait()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/RealLava/fl00dhub/main/assets/load.lua"))()
+task.wait()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/RealLava/fl00dhub/main/assets/antiafk.lua"))()
+task.wait()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/RealLava/fl00dhub/main/ui/consolejb.lua"))()
+task.wait()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/RealLava/fl00dhub/main/assets/text.lua"))()
+
+--[[ 
+This script is not our actual code, the code is loaded via our loadstrings.
+
+   $$$$$\           $$\ $$\ $$\                                     $$\       
+   \__$$ |          \__|$$ |$$ |                                    $$ |      
+      $$ | $$$$$$\  $$\ $$ |$$$$$$$\   $$$$$$\   $$$$$$\   $$$$$$\  $$ |  $$\ 
+      $$ | \____$$\ $$ |$$ |$$  __$$\ $$  __$$\ $$  __$$\  \____$$\ $$ | $$  |
+$$\   $$ | $$$$$$$ |$$ |$$ |$$ |  $$ |$$ |  \__|$$$$$$$$ | $$$$$$$ |$$$$$$  / 
+$$ |  $$ |$$  __$$ |$$ |$$ |$$ |  $$ |$$ |      $$   ____|$$  __$$ |$$  _$$<  
+\$$$$$$  |\$$$$$$$ |$$ |$$ |$$$$$$$  |$$ |      \$$$$$$$\ \$$$$$$$ |$$ | \$$\ 
+ \______/  \_______|\__|\__|\_______/ \__|       \_______| \_______|\__|  \__|
+
+--]]
